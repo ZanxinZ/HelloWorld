114e8c4388cd16606b2aa3ef38da756d7d6b5fdd
ZanxinZ <47962594+ZanxinZ@users.noreply.github.com>
2019-05-16 00:26:37 +0800

    Add files via upload

---
+++ WorkBlock/src/core/Paper.java
@@ -0,0 +1,13 @@
+package core;
+/**
+ * 纸张
+ * 用来组装：事件+地点+时间+人物
+ * @author pi
+ *
+ */
+public class Paper {
+private Event event;
+private Address address;
+private Time time;
+private People people;
+}
